9961cd305d5b2ea2fa41636de7cbef277ebfe5ae
Paras Sharma <paraskumarsharma@gmail.com>
2017-10-10 00:03:24 +0530

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser')
+var app = express();
+var routes = require('./routes/index');
+
+//configure app
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// use middleware
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + 'bower_components')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log('ready on port 3000 or ' + port);
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,137 @@
+var express = require('express');
+var router = express.Router();
+
+var Cart = require('../models/cart');
+
+var Product = require('../models/product');
+	console.log(Product.length);
+//var Order = require('../models/order');
+
+/* GET home page. */
+//define routes
+
+router.get('/', function(req, res){
+  res.render('index');
+});
+
+router.post('/proceed', function(req, res){
+  console.log('Proceed clicked');
+  var guestUser = req.body.guestUser;
+  //console.log('user : ' + guestUser);
+  res.render('welcome', {userName: guestUser});
+});
+
+router.get('/welcome', function(req, res){
+  res.render('welcome');
+});
+
+router.get('/about', function(req, res){
+  res.render('about');
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+
+router.post('/cardmall', function(req, res, next) {
+  console.log('cardmall clicked....');
+  console.log(Product.length);
+  /*******************/
+  var MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb://localhost:27017/cartmandb";
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		db.collection("creditcards").find({}).toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+			db.close();
+			res.render('cardmall', {
+				products: result
+			});
+		});
+	});
+  
+  
+  
+  /*Product.getData(function(err, data){
+    if (err) {
+      return res.redirect('/cardmall');
+    }
+	console.log('data: ');
+	console.log(data.length);
+	  res.render('cardmall', {
+		products: data
+		});
+  });
+  res.render('cardmall', {
+      products: Product
+    });*/
+  });
+
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/cardmall');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/cardmall');
+  });
+});
+
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', {
+      products: null
+    });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice
+  });
+});
+
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+
+  res.render('shop/checkout', {
+    total: cart.totalPrice,
+    noError: !errMsg
+  });
+});
+
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  console.log('is authenticated');
+    return next();
+
+}
